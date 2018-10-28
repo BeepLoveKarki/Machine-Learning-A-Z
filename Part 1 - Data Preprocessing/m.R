468f18097ee584b17120d94a989b1267ec522ba7
Biplab <biplabkarki04@gmail.com>
2018-10-28 11:41:24 +0545

    DP complete

--- Part 1 - Data Preprocessing/m.R
+++ Part 1 - Data Preprocessing/m.R
@@ -1,6 +1,26 @@
+#Data preprocessing
+
+#read data
 dataset = read.csv('Data.csv')
-dataset$Age = ifelse(is.na(dataset$Age),
-                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age)
-dataset$Salary=ifelse(is.na(dataset$Salary),ave(dataset$Salary,FUN=function(x) mean(x,na.rm=TRUE)),
-                      dataset$Salary)
+#dataset=datset[,2:3]
+
+#split test and training set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio=0.8)
+training_set=subset(dataset,split==TRUE)
+test_set=subset(dataset,split==FALSE)
+
+#feature scaling
+#training_set[,2:3]=scale(training_set[,2:3])
+#test_set[,2:3]=scale(test_set[,2:3])
+
+
+
+
+
+
+
+
+
--- Part 1 - Data Preprocessing/m.py
+++ Part 1 - Data Preprocessing/m.py
@@ -1,13 +1,24 @@
+#Data preprocssing
+
+#importing libraries
 import numpy as py
 import matplotlib.pyplot as plt
 import pandas as pd
 
+#importing dataset
 dataset=pd.read_csv('Data.csv')
 X=dataset.iloc[:,:-1].values
 Y=dataset.iloc[:,3].values
 
-from sklearn.preprocessing import Imputer
-imputer=Imputer(missing_values="NaN",strategy="mean",axis=0)
-imputer=imputer.fit(X[:,1:3])
-X[:,1:3]=imputer.transform(X[:,1:3])
 #X[:,1:3]=py.round(X[:,1:3].astype(py.double),2)
+
+
+#split into training and test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
+
+#Feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)"""
